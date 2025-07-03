3f121d9d29edb99cbeedb6f45b4bfff48acdc42c
Juan Soto <rincom1244@gmail.com>
2025-07-02 22:38:25 -0500

    feat: api

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://burger-station-backend.onrender.com',
-  baseURL: 'http://localhost:4400',
+  baseURL: 'https://burger-station-backend.onrender.com',
   withCredentials: true,
 });
 
